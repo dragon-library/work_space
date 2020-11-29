df8d53398d8078c27d2a87af9516136b97b5670c
yosarawut <yo_sarawut@hotmail.com>
2020-11-30 03:30:43 +0700

    .

---
+++ HS_Code/Lab/manage.py
@@ -0,0 +1,8 @@
+history = model.fit(X_train, y_train,                    epochs=20,                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history)
