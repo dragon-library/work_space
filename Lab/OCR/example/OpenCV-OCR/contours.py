cc2f5029db4d62d318370e8f2e59a63bed944e05
yosarawut <yo_sarawut@hotmail.com>
2021-01-27 16:58:39 +0700

    .

---
+++ Lab/OCR/example/OpenCV-OCR/contours.py
@@ -0,0 +1,6 @@
+# Finding contours 
+im_gray = gray(im)
+im_blur = blur(im_gray)
+im_thresh = threshold(im_blur)
+
+contours, _ = cv2.findContours(im_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
